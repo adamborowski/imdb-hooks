83728e7a78a66fc41dbcb7a62ed59f8b15c19f1c
Adam Borowski <fl.borovsky@gmail.com>
2019-03-17 01:15:47 +0100

    #1 Added missing dependencies

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,8 @@
-import { configure } from '@storybook/react';
+import {withKnobs} from '@storybook/addon-knobs';
+import {addDecorator, configure} from '@storybook/react';
+import 'antd/dist/antd.min.css';
+
+addDecorator(withKnobs);
 
 function loadStories() {
   const requireContext = require.context('../src', true, /\.stories.[tj]sx?/);
