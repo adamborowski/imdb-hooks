96e2861c14ad6f32c64fbae6c42c8cac94bbce32
Adam Borowski <fl.borovsky@gmail.com>
2019-03-21 13:49:46 +0100

    Updates

---
+++ precache-manifest.8f016378a02d3ad9bd859ee2f2583bd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "09ab7409afbed3c1d49d",
+    "url": "/imdb-hooks/static/js/runtime~main.8c9ae040.js"
+  },
+  {
+    "revision": "716e39bb1133a02924e8",
+    "url": "/imdb-hooks/static/js/main.587b7f73.chunk.js"
+  },
+  {
+    "revision": "556094c31b0d4c79fe9d",
+    "url": "/imdb-hooks/static/js/2.131fb748.chunk.js"
+  },
+  {
+    "revision": "556094c31b0d4c79fe9d",
+    "url": "/imdb-hooks/static/css/2.8d24f31c.chunk.css"
+  },
+  {
+    "revision": "38111d2c6bc74464e933939dfc77189e",
+    "url": "/imdb-hooks/index.html"
+  }
+];
