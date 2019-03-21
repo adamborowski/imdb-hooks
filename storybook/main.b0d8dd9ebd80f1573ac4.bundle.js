4377be3fd5e97c28f4a797aa72a061c8f61973d1
Adam Borowski <fl.borovsky@gmail.com>
2019-03-21 17:28:51 +0100

    Updates

---
+++ storybook/main.b0d8dd9ebd80f1573ac4.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,p){p(398),p(476),n.exports=p(858)},476:function(n,o,p){"use strict";p.r(o);p(477),p(700),p(855)}},[[0,1,2]]]);
---
+++ storybook/precache-manifest.173823a9b0307ddfc406f804304144d8.js
@@ -0,0 +1,31 @@
+self.__precacheManifest = [
+  {
+    "revision": "47d7cac571cdec350d55",
+    "url": "vendors~main.css"
+  },
+  {
+    "url": "vendors~main.41bff851588b28c6e899.bundle.js"
+  },
+  {
+    "revision": "519cff02ac5bd15fcd9b3f37cdf9aa41",
+    "url": "static/media/exampleAvatar.519cff02.jpg"
+  },
+  {
+    "revision": "fece421f4375ce63954991f6a01d9b12",
+    "url": "static/media/emptyAvatar.fece421f.png"
+  },
+  {
+    "revision": "5c339183772bd4f8f44184eb2c468996",
+    "url": "static/media/AppSider.logo.5c339183.png"
+  },
+  {
+    "url": "runtime~main.41bff851588b28c6e899.bundle.js"
+  },
+  {
+    "url": "main.41bff851588b28c6e899.bundle.js"
+  },
+  {
+    "revision": "86b4ad8a9c9abc7c24d0abe6f79b58b2",
+    "url": "iframe.html"
+  }
+];
